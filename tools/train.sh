6c6ea0227d611fbe0068cdb5c3234a9eeaadb1bb
jinyu <jinyuli1999@qq.com>
2023-05-05 04:21:40 -0700

    fix

--- tools/train.sh
+++ tools/train.sh
@@ -8,7 +8,7 @@ export PYTHONPATH=$PYTHONPATH:/home/jinyu/code/pillarnext/trainer
 # CUDA_LAUNCH_BLOCKING=1
 torchrun --standalone --nnodes=1 --nproc_per_node=1   \
          ./tools/train.py --config-name waymo_det_pp18_aspp_iou_car_sp \
-         dataloader.train.batch_size=4  scheduler.max_lr=0.002 trainer.max_epochs=12 \
+         dataloader.train.batch_size=2  scheduler.max_lr=0.001 trainer.max_epochs=12 \
          trainer.eval_every_nepochs=12  \
          hydra.run.dir=outputs/waymo_det_pp18_aspp_iou_car_sp \
         #  +resume_from=epoch_12.pth
